f1b83ac8043b5acd6d493b9987ef306ed715c80c
QUIGNARD Léandre <leandrequi@gmail.com>
2026-08-23 20:20:48 -0500

    Fix bug Collector

--- Assets/Scripts/Upgrades/Upgrades.cs
+++ Assets/Scripts/Upgrades/Upgrades.cs
@@ -5,6 +5,11 @@ public class Upgrades : Interectable
     public GameObject uiUpragde;
     public UnitTier unit;
 
+    private void Awake()
+    {
+        
+    }
+
     public override void Interact()
     {
         base.Interact();
